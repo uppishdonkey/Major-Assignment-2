490a8bac5d206fe7c81fe239519c5e8d5b788fcf
jbarz <jbarz0415@gmail.com>
2023-12-09 21:56:16 -0500

    added back button and adjusted functionality of js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
+// script.js
 document.addEventListener('DOMContentLoaded', function () {
     const discussionContainers = document.querySelectorAll('.content > div');
  
     discussionContainers.forEach(container => {
-       container.addEventListener('click', function (event) {
+       container.addEventListener('click', function () {
           const discussionPage = this.getAttribute('data-href');
           if (discussionPage) {
              window.location.href = discussionPage;
